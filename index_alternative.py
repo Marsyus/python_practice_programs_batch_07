a769140f7f1bbc4c7ff9d4ea9dc3d99c486b8f3c
Marsyus <miadolomboy0917@gmail.com>
2025-03-30 16:12:50 +0800

    Created function index_replicate()

--- index_alternative.py
+++ index_alternative.py
@@ -1,3 +1,5 @@
 #Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
 #Create a function to replicate index()
+def index_replicate(val, var):
+    pass
 #Utilize the created function
