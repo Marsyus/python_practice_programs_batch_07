4ad8e5b4f0d27b98795539a52c52a961cf48a39d
Marsyus <miadolomboy0917@gmail.com>
2025-03-30 16:24:16 +0800

    Remove suffix that matches parameter

--- removesuffix_alternative.py
+++ removesuffix_alternative.py
@@ -1,5 +1,9 @@
 #Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
 #Create a function to replicate removesuffix()
 def removesuffix_replicate(text, remove):
-    pass
+    if text.endswith(remove):
+        output = text.replace(remove, "")
+    else:
+        output = text
+    return output
 #Utilize the created function
