973a0bb2c8b8289b21e19de85362646a3a331fe9
Marsyus <miadolomboy0917@gmail.com>
2025-03-30 16:12:04 +0800

    Created function count_replicate()

--- count_alternative.py
+++ count_alternative.py
@@ -1,3 +1,5 @@
 #Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 #Create a function to replicate count()
+def count_replicate(val, var):
+    pass
 #Utilize the created function
