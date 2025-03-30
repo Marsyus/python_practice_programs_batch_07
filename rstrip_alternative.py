dcf8bd5803d1af041cf6c28b2f3ebec896386583
Marsyus <miadolomboy0917@gmail.com>
2025-03-30 16:05:38 +0800

    Created function rstrip_replicate()

--- rstrip_alternative.py
+++ rstrip_alternative.py
@@ -1,3 +1,5 @@
 #Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
 #Create a function to replicate rstrip()
+def rstrip_replicate(text):
+    pass
 #Utilize the created function
