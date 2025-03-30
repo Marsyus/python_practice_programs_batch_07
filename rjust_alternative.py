76f9b8dcac35b8c0095b6ec3d59f165fb79829cc
Marsyus <miadolomboy0917@gmail.com>
2025-03-31 04:16:40 +0800

    Added a default char value " "

--- rjust_alternative.py
+++ rjust_alternative.py
@@ -1,9 +1,9 @@
 #Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
 #Create a function to replicate rjust()
-def rjust_replicate(text, width, char):
+def rjust_replicate(text, width, char=" "):
     output = char * width + text
     return output
 #Utilize the created function
 name = "Marxius"
-call = rjust_replicate(name, 10, "-")
+call = rjust_replicate(name, 10)
 print(call)
