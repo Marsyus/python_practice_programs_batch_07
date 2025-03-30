c03e9ecbcb9311c2bc138f7742b844dee4f9dedf
Marsyus <miadolomboy0917@gmail.com>
2025-03-30 16:06:43 +0800

    Created function removesuffix_replicate()

--- removesuffix_alternative.py
+++ removesuffix_alternative.py
@@ -1,3 +1,5 @@
 #Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
 #Create a function to replicate removesuffix()
+def removesuffix_replicate(text, remove):
+    pass
 #Utilize the created function
