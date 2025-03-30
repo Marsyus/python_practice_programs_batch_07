f541324f3580f6f3a4ec167e29dc6400823aaacd
Marsyus <miadolomboy0917@gmail.com>
2025-03-30 22:46:45 +0800

    Forgot to set 'first' value into True

--- index_alternative.py
+++ index_alternative.py
@@ -1,7 +1,7 @@
 #Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
 #Create a function to replicate index()
 def index_replicate(val, var):
-    first = False
+    first = True
     count = -1
     for i in var:
         if first:
