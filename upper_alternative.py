c2d692957264e057ad95ee4e32af218b5aae7957
Marsyus <miadolomboy0917@gmail.com>
2025-03-30 16:25:44 +0800

    Converts all characters into uppercase

--- upper_alternative.py
+++ upper_alternative.py
@@ -1,5 +1,7 @@
 #Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 #Create a function to replicate upper()
 def upper_replicate(text):
-    pass
+    table = str.maketrans("abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    output = text.translate(table)
+    return output
 #Utilize the created function
