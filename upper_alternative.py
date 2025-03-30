deef1357cfc44b7c68f8d54bb301e36bd17a1568
Marsyus <miadolomboy0917@gmail.com>
2025-03-30 16:07:47 +0800

    Created function upper_replicate()

--- upper_alternative.py
+++ upper_alternative.py
@@ -1,3 +1,5 @@
 #Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 #Create a function to replicate upper()
+def upper_replicate(text):
+    pass
 #Utilize the created function
