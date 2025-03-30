5d980b3bca129ab35f74e444242655fc9198e071
Marsyus <miadolomboy0917@gmail.com>
2025-03-30 21:11:59 +0800

    Fills the beginning with zeros

--- zfill_alternative.py
+++ zfill_alternative.py
@@ -1,5 +1,7 @@
 #Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 #Create a function to replicate zfill()
 def zfill_replicate(num, width):
-    pass
+    need = width - len(num)
+    output = "0" * need + num
+    return output
 #Utilize the created function
