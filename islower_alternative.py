2ca1b996ed548a121127bb979f06b241edb6842f
Marsyus <miadolomboy0917@gmail.com>
2025-03-30 16:08:47 +0800

    Created function islower_replicate()

--- islower_alternative.py
+++ islower_alternative.py
@@ -1,3 +1,5 @@
 #Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
 #Create a function to replicate islower()
+def islower_replicate(text):
+    pass
 #Utilize the created function
