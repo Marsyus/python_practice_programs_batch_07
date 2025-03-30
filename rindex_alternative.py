91e331e2a75a34e10243a85f88a383d155c10fe5
Marsyus <miadolomboy0917@gmail.com>
2025-03-30 22:49:09 +0800

    Fixed function to not only include single strings

--- rindex_alternative.py
+++ rindex_alternative.py
@@ -1,12 +1,16 @@
 #Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
 #Create a function to replicate rindex()
 def rindex_replicate(val, var):
-    last = False
+    last = True
     count = len(val)
+    for i in var:
+        if last:
+            l_var = i
+            last = False
     for i in val[::-1]:
         if not last:
             count -= 1
-            if i == var:
+            if i == l_var:
                 last = True
     return count
 #Utilize the created function
