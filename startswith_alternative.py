283816c441513f6f31cf3a739e164fe654499390
Marsyus <miadolomboy0917@gmail.com>
2025-03-30 16:09:38 +0800

    Created function startswith_replicate()

--- startswith_alternative.py
+++ startswith_alternative.py
@@ -1,3 +1,5 @@
 #Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
 #Create a function to replicate startswith()
+def startswith_replicate(text, beginning):
+    pass
 #Utilize the created function
