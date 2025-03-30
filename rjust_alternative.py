d5fd2dcb0962f128c15472d1ac197a21dbe1e9a2
Marsyus <miadolomboy0917@gmail.com>
2025-03-30 21:08:48 +0800

    Adjusts text to the right

--- rjust_alternative.py
+++ rjust_alternative.py
@@ -1,5 +1,6 @@
 #Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
 #Create a function to replicate rjust()
 def rjust_replicate(text, width, char):
-    pass
+    output = char * width + text
+    return output
 #Utilize the created function
