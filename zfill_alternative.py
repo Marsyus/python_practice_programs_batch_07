8068fba9f41975d0321c57921c7443a68c71cc3c
Marsyus <miadolomboy0917@gmail.com>
2025-03-30 16:11:12 +0800

    Created function zfill_replicate()

--- zfill_alternative.py
+++ zfill_alternative.py
@@ -1,3 +1,5 @@
 #Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 #Create a function to replicate zfill()
+def zfill_replicate(num, width):
+    pass
 #Utilize the created function
